13212af1ae6477bc3996874bb0b8d4ac78a0099d
Himanshu <hk6691222@gmail.com>
2021-07-27 23:04:36 +0530

    first commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component.
+            <p>This is an about paragraph for 
+                showing a page of mycomponents 
+                of the todos-list to learn 
+                React-Javascript.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut quia porro inventore ratione itaque sit dolorum mollitia tempora delectus optio explicabo dignissimos voluptatibus repellendus, aliquam expedita omnis! Officiis consectetur, totam facilis sunt magnam omnis.</p>
+        </div>
+    )
+}
