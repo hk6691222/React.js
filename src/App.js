2d901c7b940cd2032b79e0f56f47913a78a8da91
HimanshuKumar <hkumar506070@gmail.com>
2024-06-16 15:41:50 +0530

    first message

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import { Todos } from "./MyComponents/Todos";
 import { AddTodo } from "./MyComponents/AddTodo";
 import { About } from "./MyComponents/About";
 import React, { useState, useEffect } from "react";
-import { 
-  BrowserRouter as Router, 
-  Switch, 
-  Route, 
-} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   let initTodo;
@@ -60,14 +56,18 @@ function App() {
       <Router>
         <Header title="My Todos List" searchBar={false} />
         <Switch>
-          <Route exact path="/" render={()=>{
-            return(
-            <>
-            <AddTodo addTodo={addTodo} />
-            <Todos todos={todos} onDelete={onDelete} />
-            </>)
-          }}>
-          </Route>
+          <Route
+            exact
+            path="/"
+            render={() => {
+              return (
+                <>
+                  <AddTodo addTodo={addTodo} />
+                  <Todos todos={todos} onDelete={onDelete} />
+                </>
+              );
+            }}
+          ></Route>
           <Route exact path="/about">
             <About />
           </Route>
--- src/MyComponents/Todos.js
+++ src/MyComponents/Todos.js
@@ -8,11 +8,10 @@ export const Todos = (props) => {
     }
     return (
         <div className="container my-3" style={myStyle}>
-           <h3 className="my-3">Todos List</h3>
-           {props.todos.length===0? "No Todos to display":
-           props.todos.map((todo)=>{
-               return (<TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>
-               )
+        <h3 className="my-3">Todos List</h3>
+            {props.todos.length===0? "No Todos to display":
+            props.todos.map((todo)=>{
+                return (<TodoItem todo={todo} key={todo.sno} onDelete={props.onDelete}/>)
             })
             }
         </div>
